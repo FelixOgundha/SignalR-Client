06995c3ea9a8086c245e5430fb572569bfbbd899
FelixOgundha <fogundha@strathmore.edu>
2022-10-04 22:45:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [employees, setEmployees] = useState([])
+  const latestEmployees = useRef(null);
+
+  latestEmployees.current = employees;
+
+  const getEmployees = () => {
+    fetch("https://localhost:7083/api/Employees")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setEmployees(result)
+        }).catch((error) => {
+          console.log(error)
+        }
+        )
+  }
+  useEffect(() => {
+    getEmployees()
+    const connection = new HubConnectionBuilder()
+      .withUrl('https://localhost:7083/hubs/employees')
+      .withAutomaticReconnect()
+      .build();
+
+    connection.start()
+      .then(result => {
+        console.log('Connected!');
+
+        connection.on('ReceiveNewEmployee', employee => {
+          const updatedEmployees = [...latestEmployees.current];
+          updatedEmployees.push(employee);
+
+          setEmployees(updatedEmployees);
+        });
+      })
+      .catch(e => console.log('Connection failed: ', e));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Department</th>
+            <th scope="col">Position</th>
+            <th scope="col">Salary</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {
+            employees.map((item, key) =>
+              <tr>
+                <th scope="row" key={key}>{key + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.department}</td>
+                <td>{item.position}</td>
+                <td>{item.salary}</td>
+              </tr>
+            )
+          }
+
+        </tbody>
+      </table>
     </div>
   );
 }
